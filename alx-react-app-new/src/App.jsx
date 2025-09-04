5d800bc4da09c132193c0ccd7c9c2d63abe11047
Felistus <fellymuia2@gmail.com>
2025-09-04 21:39:46 +0300

    Added alx-react-app-new as a normal folder with all files

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -2,12 +2,14 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile'; // ✅ Import
+import Counter from './components/ounter';
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+     <Counter/>
       <UserProfile 
         name="Alice" 
         age="25" 
