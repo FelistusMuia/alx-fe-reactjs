ccdebdb42720cee86fbbd6bb4905128d1b5456f2
Felistus <fellymuia2@gmail.com>
2025-09-06 09:41:37 +0300

    Added my-company project with React Router setup

---
+++ my-company/src/App.jsx
@@ -0,0 +1,34 @@
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Services from "./pages/Services";   //  new import
+import Navbar from "./pages/Navbar";
+
+function App() {
+  return (
+    <div>
+      <h1>My Website</h1>
+
+      {/* Navigation */}
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/services">Services</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />   {/* new route */}
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<h2>Page Not Found</h2>} />
+        <Route path="/Navbar" element={<Navbar />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
