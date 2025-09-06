e65d421e9948a973d7a8d6b3ded07d277dda1ff6
Felistus <fellymuia2@gmail.com>
2025-09-06 09:55:59 +0300

    Added my-company project with React Router setup

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <ul style={{ display: "flex", gap: "1rem", listStyle: "none" }}>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+    <nav style={{ padding: "10px", backgroundColor: "#eee" }}>
+      <ul style={{ display: "flex", gap: "20px", listStyle: "none" }}>
+        <li><Link to="/" style={{ textDecoration: "none" }}>Home</Link></li>
+        <li><Link to="/about" style={{ textDecoration: "none" }}>About</Link></li>
+        <li><Link to="/services" style={{ textDecoration: "none" }}>Services</Link></li>
+        <li><Link to="/contact" style={{ textDecoration: "none" }}>Contact</Link></li>
       </ul>
     </nav>
   );
