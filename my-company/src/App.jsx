d88928df8628d3b4f243682935765732d759976a
Felistus <fellymuia2@gmail.com>
2025-09-06 10:02:40 +0300

    Added Navbar component and routing in App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,33 +1,29 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import Services from "./pages/Services";   //  new import
+import Services from "./pages/Services";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <h1>My Website</h1>
+    <Router>
+      <div>
+        <h1>My Website</h1>
 
-      {/* Navigation */}
-      <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
-        <Link to="/services">Services</Link> |{" "}
-        <Link to="/contact">Contact</Link>
-      </nav>
+        {/* Navigation */}
+        <Navbar />
 
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />   {/* new route */}
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<h2>Page Not Found</h2>} />
-        <Route path="/Navbar" element={<Navbar />} />
-      </Routes>
-    </div>
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<h2>Page Not Found</h2>} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
