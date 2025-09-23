808a5eb7a38ec5a90d7cce6129c7a7ddfe6a602c
Felistus <fellymuia2@gmail.com>
2025-09-23 21:07:25 +0300

    TailwindCSS Responsive Design

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,18 +1,26 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+    <div className="
+      user-profile 
+      bg-gray-100 
+      p-8 md:p-8 sm:p-4 
+      max-w-sm md:max-w-sm sm:max-w-xs 
+      mx-auto my-20 
+      rounded-lg shadow-lg
+    ">
       <img 
-      className="w-36 h-36 rounded-full mx-auto"
-      src="https://via.placeholder.com/150" 
-      alt="User" />
+        className="w-36 h-36 rounded-full mx-auto sm:w-24 h-24 md:w-36 h-36 "
+        src="https://via.placeholder.com/150" 
+        alt="User" 
+      />
 
-      <h1 className="text-xl my-4 text-blue-800">John Doe</h1>
-      <p className="text-gray-600 text-base text-center">
+      <h1 className="sm:text-lg md:text-xl my-4 text-blue-800 text-center">
+        John Doe
+      </h1>
+      <p className="sm:text-sm md:text-base text-gray-600 text-center">
         Developer at Example Co. Loves to write code and explore new technologies
-    </p>
-
+      </p>
     </div>
-  
   );
 }
 
