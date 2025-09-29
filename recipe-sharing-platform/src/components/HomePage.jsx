e59e3a0ca1f6969f28fdda5297ab787af151e559
Felistus <fellymuia2@gmail.com>
2025-09-29 21:08:46 +0300

    Setting Up a React Application for a Recipe Sharing Platform with Tailwind CSS

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -16,7 +16,7 @@ function HomePage() {
       </h1>
 
       {/* Grid of cards */}
-      <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
         {recipes.map((recipe) => (
           <div
             key={recipe.id}
