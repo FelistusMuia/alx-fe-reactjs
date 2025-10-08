27b73b0e48649d60cc39da78cf4168c8f4f4a716
Felistus <fellymuia2@gmail.com>
2025-10-08 21:16:58 +0300

    Advanced Routing in React with React Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
@@ -22,6 +23,9 @@ function App() {
             </ProtectedRoute>
           }
         />
+
+        {/*  Dynamic Route for Blog Posts */}
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
